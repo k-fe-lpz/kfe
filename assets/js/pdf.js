b193e7bab4dac3d7d5a4759a6129bee6ad620556
Diego Mendoza <diegom.mendoza2601@gmail.com>
2024-07-26 15:52:24 -0400

    pdfjs testing

---
+++ assets/js/pdf.js
@@ -0,0 +1,35 @@
+//prettier-ignore
+initPDFViewer = () => {
+
+  $("#pdfViewerDiv").html("");
+
+  pdfjsLib.getDocument("https://k-fe-lpz.github.io/kfe/menu_2.pdf").promise.then((pdfDoc) => {
+    let pages = pdfDoc._pdfInfo.numPages;
+
+    for(let i = 1; i <= pages; i ++){
+      pdfDoc.getPage(i).then((page) => {
+      console.log(page);
+      let pdfCanvas = document.createElement("canvas");
+      let context = pdfCanvas.getContext("2d");
+      let pageViewPort = page.getViewport({scale:1});
+      console.log(pageViewPort);
+
+      pdfCanvas.width = pageViewPort.width;
+      pdfCanvas.height = pageViewPort.height;
+      $("#pdfViewerDiv").append(pdfCanvas);
+
+      page.render({
+        canvasContext: context,
+        viewport: pageViewPort,
+      });
+
+    })
+    .catch((error) => console.log(error));
+    }
+    
+  }).catch((error) => console.log(error));
+};
+
+$(function () {
+  initPDFViewer();
+});
